e6bb984b8456e3478d87e60da49275c09b25ec61
Diego Duarte <duartqx@gmail.com>
2024-04-12 01:08:14 -0300

    tests with file picker

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
 	mux.HandleFunc("/play", playerHandler)
+	mux.HandleFunc("/form", formHandler)
 	if err := http.ListenAndServe(":8088", mux); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
 }
 
 func playerHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +39,13 @@ func playerHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("cmd waited, OK")
 
 	w.Write(([]byte("OK")))
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	f := r.Form["filesNames"]
+	log.Println("\n", f, len(f), "\n ")
 }
